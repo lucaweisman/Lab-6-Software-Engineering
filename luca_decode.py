a11029de08cc182ca4d45b8836c65880ae5d038d
yurishin <myshin0805@gmail.com>
2023-10-25 15:39:08 -0400

    mia shin - decode method

--- luca_decode.py
+++ luca_decode.py
@@ -1,12 +1,23 @@
 # luca weisman
 
-def decode(encoded_password):
-  original_password = ""
-  for digit in encoded_password:
+# def decode(encoded_password):
+ # original_password = ""
+  #for digit in encoded_password:
+   # temp = int(digit) - 3
+   # if temp < 0:
+    #  temp += 10
+    #  original_password += str(temp)
+    #else:
+    #  original_password += str(temp)
+  #return original_password
+
+def decode(number):
+  decoded_num = ""
+  for digit in number:
     temp = int(digit) - 3
     if temp < 0:
-      temp += 10
-      original_password += str(temp)
+      decoded_num += str(10 - temp)
     else:
-      original_password += str(temp)
-  return original_password
+      decoded_num += str(temp)
+    return decoded_num
+
